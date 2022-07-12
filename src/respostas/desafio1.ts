b6c9a9bb6666498c83a4c765eccd8aff5a8f2dd2
Daniel Serrão <84791388+daniiserrao@users.noreply.github.com>
2022-07-12 15:24:53 -0300

    Add files via upload

---
+++ src/respostas/desafio1.ts
@@ -0,0 +1,13 @@
+interface Funcionario {  // Já conhece interfaces? https://blog.logrocket.com/types-vs-interfaces-in-typescript/
+    codigo: number,
+    nome: string
+};
+
+const funcionarioObj = {} as Funcionario;
+funcionarioObj.codigo = 10;
+funcionarioObj.nome = 'João';
+
+const funcionarioObj2: Funcionario = {
+    codigo: 10,
+    nome: 'João'
+}
